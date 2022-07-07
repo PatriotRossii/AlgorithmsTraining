0a4b0bdb54ad883abbc87f1a0020a63e4f9b091e
PatriotRossii <patriotrossii2019@mail.ru>
2022-07-07 14:02:21 +0500

    [1B] Fix tests, solution for Даты

--- Занятие 1/B/C. Даты/solution.hpp
+++ Занятие 1/B/C. Даты/solution.hpp
@@ -1,4 +1,4 @@
 bool is_sufficient(int x, int y, int z) {
-    if(x > 12 || y > 12) return 1;
+    if(x > 12 || y > 12 || x == y) return 1;
     return 0;
 }
--- Занятие 1/B/C. Даты/test.cpp
+++ Занятие 1/B/C. Даты/test.cpp
@@ -5,7 +5,6 @@ int main() {
     assert(is_sufficient(1, 2, 2003) == 0);
     assert(is_sufficient(2, 29, 2008) == 1);
 
-    assert(is_sufficient(12, 12, 2004) == 0);
     assert(is_sufficient(13, 12, 2004) == 1);
     assert(is_sufficient(12, 13, 2004) == 1);
 
